3475ed527597105360c0063c4d5b1abd2b1a71d9
MonikaTychy <mo.tychy@gmail.com>
2022-07-31 22:01:39 +0200

    part1 ex1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,9 +9,7 @@ const Total = (props) => {
 const Content = (props) => {
   return (
     <div>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
+    <p>{props.part} {props.exercises}</p>
     </div>
   );
 }
@@ -34,8 +32,9 @@ const App = () => {
   return (
     <div className="App">
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-  
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
       <Total exerciseSum={exercises1 + exercises2 + exercises3}/>
       
     </div>
